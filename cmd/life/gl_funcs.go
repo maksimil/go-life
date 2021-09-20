8d31d966d00fcb4ff1bff4e796fbdc45b2d38c4d
Maxim Kosterov <maxim.kosterov@gmail.com>
2021-09-21 02:17:39 +0300

    improved code distribution

---
+++ cmd/life/gl_funcs.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"unsafe"
+
+	gl "github.com/go-gl/gl/v3.2-core/gl"
+	glfw "github.com/go-gl/glfw/v3.3/glfw"
+)
+
+func initGLFW() *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.ContextVersionMinor, 2)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(width, height, "Conway's Game of Life", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+
+	return window
+}
+
+// initOpenGL initializes OpenGL and returns an intiialized program.
+func initOpenGL() {
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	log.Println("OpenGL version", version)
+}
+
+func mkvbo(data []float32) uint32 {
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(data), gl.Ptr(data), gl.STATIC_DRAW)
+	return vbo
+}
+
+func mkvao() uint32 {
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	return vao
+}
+
+func vertAttribPtr(prog uint32, name string, size int32, xtype uint32, stride int32, offset uintptr) {
+	idx := uint32(gl.GetAttribLocation(prog, gl.Str(name)))
+	gl.EnableVertexAttribArray(idx)
+	gl.VertexAttribPointer(idx, size, xtype, false, stride, unsafe.Pointer(offset))
+}
+
+func compileShader(source string, stype uint32) (uint32, error) {
+	shader := gl.CreateShader(stype)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength)+1)
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
